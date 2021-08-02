6c608b55067ce426d9f128e8145a53eb55128558
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-02 19:49:55 -0300

    criando projeto e tela principal

---
+++ calculadora/src/br/com/ksg/view/Main.java
@@ -0,0 +1,20 @@
+package br.com.ksg.view;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+	public Main() {
+		
+		setLocationRelativeTo(null);
+		setSize(232, 322);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
