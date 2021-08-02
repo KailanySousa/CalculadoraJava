85ddad4bf4a6262189be2e909d119b0dcc23710c
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-02 20:16:24 -0300

    estilizando botao

--- calculadora/src/br/com/ksg/view/Botao.java
+++ calculadora/src/br/com/ksg/view/Botao.java
@@ -1,11 +1,69 @@
 package br.com.ksg.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Botao extends JButton {
 
-	public Botao() {
-		// TODO Auto-generated constructor stub
+	private String texto;
+	private Color corBotao;
+	private Color corTexto;
+	
+	public Botao(String texto, Color corBotao) {
+
+		this.setTexto(texto);
+		this.setCorBotao(corBotao);
+		this.setCorTexto(Color.WHITE);
+		
+		setOpaque(true);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		setFont(new Font("courier", Font.PLAIN, 25));
+		
+	}
+	
+	public Botao(String texto, Color corBotao, Color corTexto) {
+
+		this.setTexto(texto);
+		this.setCorBotao(corBotao);
+		this.setCorTexto(corTexto);
+		
+		setOpaque(true);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		setFont(new Font("courier", Font.PLAIN, 25));
+		
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+		setText(this.texto);
+	}
+
+	public Color getCorBotao() {
+		return corBotao;
+	}
+
+	public void setCorBotao(Color corBotao) {
+		this.corBotao = corBotao;
+		setOpaque(true);
+		setBackground(this.corBotao);
+	}
+
+	public Color getCorTexto() {
+		return corTexto;
+	}
+
+	public void setCorTexto(Color corTexto) {
+		this.corTexto = corTexto;
+		setForeground(this.corTexto);
 	}
 
 }
--- calculadora/src/br/com/ksg/view/Teclado.java
+++ calculadora/src/br/com/ksg/view/Teclado.java
@@ -1,11 +1,44 @@
 package br.com.ksg.view;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 public class Teclado extends JPanel {
+	
+	private final Color BG_OUTROS = new Color(68, 68, 68);
+	private final Color BG_NUMEROS = new Color(99, 99, 99);
+	private final Color BG_OPERACOES = new Color(242, 163, 60);
+	private final Color TEXTO = new Color(255,255,255);
 
 	public Teclado() {
 		
+		setLayout(new GridLayout(5,4));
+		
+		add(new Botao("AC", BG_OUTROS));
+		add(new Botao("+/-", BG_OUTROS));
+		add(new Botao("%", BG_OUTROS));
+		add(new Botao("/", BG_OPERACOES));
+		
+		add(new Botao("7", BG_NUMEROS));
+		add(new Botao("8", BG_NUMEROS));
+		add(new Botao("9", BG_NUMEROS));
+		add(new Botao("*", BG_OPERACOES));
+		
+		add(new Botao("4", BG_OUTROS));
+		add(new Botao("5", BG_OUTROS));
+		add(new Botao("6", BG_OUTROS));
+		add(new Botao("+", BG_OPERACOES));
+		
+		add(new Botao("1", BG_OUTROS));
+		add(new Botao("2", BG_OUTROS));
+		add(new Botao("3", BG_OUTROS));
+		add(new Botao("-", BG_OPERACOES));
+		
+		add(new Botao("0", BG_OUTROS));
+		add(new Botao(".", BG_OUTROS));
+		add(new Botao("=", BG_OPERACOES));
 	}
 
 }
