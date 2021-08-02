55e677acbb6e95d869dc39672297f115f70ed724
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-02 20:01:51 -0300

    estilizando display

--- calculadora/src/br/com/ksg/view/Display.java
+++ calculadora/src/br/com/ksg/view/Display.java
@@ -1,11 +1,34 @@
 package br.com.ksg.view;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Display extends JPanel {
+	
+	private JLabel label = new JLabel();
 
 	public Display() {
 		
+		setBackground(new Color(46, 49,50));
+		this.label.setForeground(Color.WHITE);
+		this.label.setFont(new Font("courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		
+		add(this.label);
+		
+	}
+
+	public JLabel getLabel() {
+		return label;
+	}
+
+	public void setLabel(JLabel label) {
+		this.label = label;
 	}
 
 }
