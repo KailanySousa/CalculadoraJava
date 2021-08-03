05764fbf9521198a4eb01f9733e078033a09bd4c
KailanySousa <kailany-sousa2010@hotmail.com>
2021-08-02 22:43:18 -0300

    componente teclado #02

--- calculadora/src/br/com/ksg/view/Teclado.java
+++ calculadora/src/br/com/ksg/view/Teclado.java
@@ -19,27 +19,40 @@ public class Teclado extends JPanel {
 		
 		setLayout(layout);
 		
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		
+		// LINHA 1
+		c.gridwidth = 3;
 		this.adicionarBotao("AC", BG_OUTROS, c, 0, 0);
-		this.adicionarBotao("+/-", BG_OUTROS, c, 1 , 0);
-		this.adicionarBotao("%", BG_OUTROS, c, 2, 0);
+		
+		c.gridwidth = 1;
 		this.adicionarBotao("/", BG_OPERACOES, c, 3, 0);
 	
+		// LINHA 2
 		this.adicionarBotao("7", BG_NUMEROS, c, 0, 1);
 		this.adicionarBotao("8", BG_NUMEROS, c, 1, 1);
 		this.adicionarBotao("9", BG_NUMEROS, c, 2, 1);
 		this.adicionarBotao("*", BG_OPERACOES, c, 3, 1);
 		
+		// LINHA 3
 		this.adicionarBotao("4", BG_NUMEROS, c, 0, 2);
 		this.adicionarBotao("5", BG_NUMEROS, c, 1, 2);
 		this.adicionarBotao("6", BG_NUMEROS, c, 2, 2);
 		this.adicionarBotao("-", BG_OPERACOES, c, 3, 2);
 		
+		// LINHA 4
 		this.adicionarBotao("1", BG_NUMEROS, c, 0, 3);
 		this.adicionarBotao("2", BG_NUMEROS, c, 1, 3);
 		this.adicionarBotao("3", BG_NUMEROS, c, 2, 3);
 		this.adicionarBotao("+", BG_OPERACOES, c, 3, 3);
 		
+		// LINHA 5
+		c.gridwidth = 2;
 		this.adicionarBotao("0", BG_NUMEROS, c, 0, 4);
+		
+		c.gridwidth = 1;
 		this.adicionarBotao(",", BG_NUMEROS, c, 2, 4);
 		this.adicionarBotao("=", BG_OPERACOES, c, 3,4);
 		
